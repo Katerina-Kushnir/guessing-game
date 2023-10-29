2d26dbadd4f0a1362562b85c9fed190cb676badd
Katerina-Kushnir <93322468+Katerina-Kushnir@users.noreply.github.com>
2023-10-29 14:24:18 +0200

    result task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.solution = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
-    }
+        this.solution = Math.ceil((this.min + this.max) / 2);
+        return this.solution;
+    }    
 
     lower() {
-
+        this.max = this.solution;
     }
 
     greater() {
-
+        this.min = this.solution;
     }
 }
 
 module.exports = GuessingGame;
+
+
